f39aaf84f346930b4111fc7344a66939dcd6247a
eder.bevacqua@gmail.com <eder.bevacqua@gmail.com>
2019-04-16 18:07:52 -0300

    melhorias boas praticas

--- javaScript/js.js
+++ javaScript/js.js
@@ -1,21 +1,27 @@
 document.getElementById("imgLogo").addEventListener("click", function () { console.log("Logo Clicado!"); });
 
-function ul_li(vl) {
-    var v = vl.getAttribute("data-value");
-    alert("Item Desordenado: " + v + ".");
-}
+var lDeso = document.querySelectorAll("div ul li");
+lDeso.forEach(function (ul) {
+    ul.addEventListener("click", function () {
+        let v = this.getAttribute("data-value");
+        alert("Item Desordenado: " + v + ".");
+    });
+});
 
-function ol_li(vl) {
-    var v = vl.getAttribute("data-value");
-    alert("Item Ordenado: " + v + ".");
-}
-
+var lOrd = document.querySelectorAll("div ol li");
+lOrd.forEach(function (ol) {
+    ol.addEventListener("click", function () {
+        let v = this.getAttribute("data-value");
+        alert("Item Ordenado: " + v + ".");
+    });
+});
 
 var formulario = document.getElementById("form1");
 var nome = document.getElementById("nome");
 var corfavorita = document.getElementById("corFavorita");
 var nascimento = document.getElementById("dNascimento");
 var salario = document.getElementById("salario");
+
 
 function focusInput(a) {
     var attNome = a.getAttribute("name")
@@ -26,7 +32,7 @@ function focusInput(a) {
                 break;
             }
             else {
-                break;
+                corfavorita.focus();
             }
         case "corFavorita":
             if (corfavorita.value == "") {
@@ -42,7 +48,7 @@ function focusInput(a) {
                 break;
             }
             else {
-                break;
+                salario.focus;
             }
         case "salario":
             if (salario.value == "") {
@@ -50,7 +56,8 @@ function focusInput(a) {
                 break;
             }
             else {
-                break;
+                let s = document.getElementsByName("Submit")
+                s.focus()
             }
     }
 
@@ -62,7 +69,7 @@ function validaform() {
         alert("Informe o seu nome.");
         return false;
     }
-    if (corFavorita.value =="") {
+    if (corFavorita.value == "") {
         alert("Informe uma cor valida.");
         return false;
     }
@@ -75,19 +82,19 @@ function validaform() {
         return false;
     }
 
-    var tabela = document.getElementById("tabelaForm");
-    var tr1 = document.createElement("tr");
-    var conteudoNovoNome = document.createTextNode(nome.value);
-    var conteudoNovoCor = document.createTextNode(corfavorita.value);
-    var conteudoNovoNasc = document.createTextNode(nascimento.value.split('-').reverse().join('/'));
-    var conteudoNovoSalar = document.createTextNode(salario.value);
+    let tabela = document.getElementById("tabelaForm");
+    let tr1 = document.createElement("tr");
+    let conteudoNovoNome = document.createTextNode(nome.value);
+    let conteudoNovoCor = document.createTextNode(corfavorita.value);
+    let conteudoNovoNasc = document.createTextNode(nascimento.value.split('-').reverse().join('/'));
+    let conteudoNovoSalar = document.createTextNode(salario.value);
 
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
-    var td4 = document.createElement("td");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
 
-    var l = document.getElementById("tabelaForm").getElementsByTagName("tr").length;
+    let l = document.getElementById("tabelaForm").getElementsByTagName("tr").length;
 
     if (l <= 5) {
         td1.appendChild(conteudoNovoNome);
@@ -103,24 +110,33 @@ function validaform() {
     else {
         alert("Você não pode adicionar mais dados");
     }
-    tableList()
-    document.getElementById("form1").reset();
-    return false;
-}
 
-function tableList() {
-    var lexcl = document.getElementById("tabelaForm").getElementsByTagName("tr");
-    for (var i = 0; i < lexcl.length; i++) {
-        lexcl[i].addEventListener("click", exclRow)
-        function exclRow(e) {
+    let lexcl = document.querySelectorAll("table tr")
+    lexcl.forEach(function (l) {
+        l.addEventListener("click", exclRow)
+        function exclRow() {
             if (this != lexcl[0]) {
                 this.parentNode.removeChild(this);
             }
         }
+    });
+    
+    document.getElementById("form1").reset();
+    return false;
+}
+
+let lexcl = document.querySelectorAll("table tr")
+for (let i = 0; i < lexcl.length; i++) {
+    lexcl[i].addEventListener("click", exclRow)
+    function exclRow() {
+        if (this != lexcl[0]) {
+            this.parentNode.removeChild(this);
+        }
     }
 }
 
-function blockEnterForm() {
-    return !(window.event && window.event.keyCode == 13);
+function blockEnterTabForm() {
+    return !(window.event && window.event.keyCode == 13 || window.event && window.event.keyCode == 9)
 }
+
 
